fb273c76dca0259b82b4e18ee580bb3484d27e47
Alexander Muratidi <intermyr@gmail.com>
2020-08-30 10:51:40 +0300

    change

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-export const apiKey = 'fe4f7d792f064521955c9ed2878a21ba'
+// export const apiKey = 'fe4f7d792f064521955c9ed2878a21ba'
 // export const apiKey = '6e66c889a30146bab6532b4d792005d5'
-// export const apiKey = '31d5d8327e9545a7926af04979e38250'
+export const apiKey = '31d5d8327e9545a7926af04979e38250'
