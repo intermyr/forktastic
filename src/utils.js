37a27b97734b35ddae2ec5d780f04815beabc5ea
Alexander Muratidi <intermyr@gmail.com>
2020-08-30 10:51:33 +0300

    fix too accurate amounts

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const round = (number) => {
   if (number > 1) {
-    return (Math.round(parseFloat(number) + Number.EPSILON) * 10) / 10;
-  } else return number
+    return Math.round((parseFloat(number) + Number.EPSILON) * 10) / 10;
+  } else return Math.trunc((parseFloat(number)) * 100) / 100
 };
